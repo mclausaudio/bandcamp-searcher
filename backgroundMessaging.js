f15f2fa99206e0b6618596c327be53ab0a6c57f5
Michael_Claus <mclausaudio@gmail.com>
2020-04-13 22:25:27 -0700

    add search by username

--- backgroundMessaging.js
+++ backgroundMessaging.js
@@ -1,3 +1,16 @@
+chrome.runtime.onMessage.addListener((req, sender, res) => {
+	console.log(req.searchTerm);
+	let searchTerm = req.searchTerm.replace(/[^A-Za-z0-9 ]/g, "");
+	console.log(searchTerm);
+	openBandcampSearch(searchTerm);
+});
+
+const openBandcampSearch = (term) => {
+	chrome.tabs.create({
+		url: `https://bandcamp.com/search?q=${term}`,
+	});
+};
+
 // chrome.runtime.onMessage.addListener((request, sender, response) => {
 //   console.log(request);
 //   console.log(sender);
--- content.js
+++ content.js
@@ -1,8 +1,20 @@
 chrome.runtime.onMessage.addListener((req, sender, res) => {
-  let title = document.querySelectorAll('.playbackSoundBadge__title a span')[1].innerText;
+	let searchTerm;
 
-  console.log(title)
-})
+	if (req.searchType === "trackTitle") {
+		console.log("inside trackTitle");
+		searchTerm = document.querySelectorAll(
+			".playbackSoundBadge__titleContextContainer"
+		)[0].children[0].innerText;
+	} else {
+		console.log("inside username");
+		searchTerm = document.querySelectorAll(
+			".playbackSoundBadge__title a span"
+		)[1].innerText;
+	}
+
+	chrome.runtime.sendMessage({ searchTerm: searchTerm });
+});
 
 // window.onload = () => {
 //   const button = document.createElement('button');
